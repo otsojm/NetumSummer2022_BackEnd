d36123dcd599a1ff5b4fa620d049e72778473378
Otso Metsäportti <62076266+otsojm@users.noreply.github.com>
2022-01-31 04:54:11 +0200

    Import cleaning

--- src/main/java/netumbackend/summer2022/domain/PersonRepository.java
+++ src/main/java/netumbackend/summer2022/domain/PersonRepository.java
@@ -1,6 +1,5 @@
 package netumbackend.summer2022.domain;
 
-import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
